c2c77a7479b6e2493d68cde6beb931462002fe4b
Daniel Enrique <dcordb97@gmail.com>
2020-11-19 02:12:40 -0500

    Add principal design

--- app/gui/mainwindow.py
+++ app/gui/mainwindow.py
@@ -2,11 +2,74 @@ from PySide2 import QtWidgets as qtw
 from PySide2 import QtGui as qtg
 from PySide2 import QtCore as qtc
 
+from matplotlib.figure import Figure
+from app.gui.mpl import FacilitiesCanvas, FacilitiesToolbar
+
+from app.gui.resources import resources
+
+class ActionWidget(qtw.QWidget):
+    '''
+    Bottom widget holding action buttons
+    '''
+
+    def __init__(self) -> None:
+        qtw.QWidget.__init__(self)
+
+        self.setLayout(qtw.QHBoxLayout())
+
+        btnLoad = qtw.QPushButton('&Load')
+        btnSave = qtw.QPushButton('&Save')
+        labInfo = qtw.QLabel('Information...')
+        btnHelp = qtw.QPushButton('&Help')
+
+        self.layout().addWidget(btnLoad)
+        self.layout().addWidget(btnSave)
+        self.layout().addWidget(labInfo)
+        self.layout().addWidget(btnHelp)
+
+class CentralWidget(qtw.QWidget):
+    '''
+    Central widget formed by a canvas and an action widget
+    '''
+
+    def __init__(self, canvas) -> None:
+        qtw.QWidget.__init__(self)
+
+        self.setLayout(qtw.QVBoxLayout())
+        self.layout().addWidget(canvas)
+
+        actionWidget = ActionWidget()
+        self.layout().addWidget(actionWidget)
+
+        self.layout().setContentsMargins(0, 0, 0, 0)
+
 class MainWindow(qtw.QMainWindow):
+    '''
+    Main Window
+    '''
+
     def __init__(self) -> None:
         qtw.QMainWindow.__init__(self)
-        self.setupGUI()
+        self.loadCSS()
+
+        self.setWindowTitle('Locating Facilities')
+
+        canvas = FacilitiesCanvas(Figure())
+        canvas.updateCanvas()
+
+        toolbar = FacilitiesToolbar(canvas)
+
+        self.addToolBar(toolbar)
+        self.setCentralWidget(CentralWidget(canvas))
+
         self.show()
 
-    def setupGUI(self):
-        pass
+    def loadCSS(self):
+        f = qtc.QFile(':/style.css')                            
+        f.open(qtc.QFile.ReadOnly | qtc.QFile.Text)
+
+        ts = qtc.QTextStream(f)
+        css = ts.readAll()
+        self.setStyleSheet(css)
+
+        f.close()
--- app/gui/mpl.py
+++ app/gui/mpl.py
@@ -5,9 +5,10 @@ from PySide2 import QtCore as qtc
 from matplotlib.backends.backend_qt5agg import FigureCanvas
 from matplotlib.backend_bases import NavigationToolbar2
 import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 import numpy as np
 
-from app.models.utils import PointW
+from app.models.utils import Point, PointW
 from typing import List
 
 class FacilitiesCanvas(FigureCanvas):
@@ -15,8 +16,16 @@ class FacilitiesCanvas(FigureCanvas):
     Represents a canvas to plot facilities into.
     '''
 
-    def __init__(self, near: List[PointW], far: List[PointW]) -> None:
-        pass
+    def __init__(self, fig: Figure) -> None:
+        FigureCanvas.__init__(self, fig)
+
+    def updateCanvas(self, near: List[PointW] = [], far: List[PointW] = [], *sols) -> None:
+        x = [0, 1, 2, 3, 2, 2]
+        y = [0, 2, 4, 1, 3, 3]
+
+        self.ax = self.figure.subplots()
+        self.ax.scatter(x, y)
+        self.draw()
 
     def mplMouseMove(self, event):
         pass
@@ -26,19 +35,20 @@ class FacilitiesToolbar(NavigationToolbar2, qtw.QToolBar):
     Represents the toolbar to work with facilities canvas plot.
     '''
 
-    def __init__(self, canvas, parent) -> None:
-        pass
+    def __init__(self, canvas) -> None:
+        qtw.QToolBar.__init__(self)
+        NavigationToolbar2.__init__(self, canvas)
 
     # functions required by MPL to be implemented
 
-    def set_cursor(self):
-        pass
+    # def set_cursor(self, cursor):
+    #     pass
 
-    def draw_rubberband(self):
-        pass
+    # def draw_rubberband(self):
+    #     pass
 
-    def set_message(self):
-        pass
+    # def set_message(self):
+    #     pass
 
-    def set_history_buttons(self):
-        pass
+    # def set_history_buttons(self):
+    #     pass
