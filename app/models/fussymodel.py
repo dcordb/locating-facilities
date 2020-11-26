3593e5a0190e08efbd02bf55d05b34bab4b7258e
Daniel Enrique <dcordb97@gmail.com>
2020-11-26 03:32:09 -0500

    Finish fussymodel

--- app/models/fussymodel.py
+++ app/models/fussymodel.py
@@ -23,7 +23,53 @@ def optimize(near: List[PT], far: List[PT], xmin: float, xmax: float, ymin: floa
     L1 = evalZ1(near, X1, Y1)
     L2 = evalZ2(xca, yca, xcb, ycb, X2, Y2)
 
-    return PT(21, 63, w=0, kind='FussyModel'), 2346.6454646
+    prob = pulp.LpProblem('FussyModel', pulp.LpMaximize)
+
+    # for first model
+    ps = [ pulp.LpVariable(f'p_{i}') for i in range(len(near)) ]
+    qs = [ pulp.LpVariable(f'q_{i}') for i in range(len(near)) ]
+
+    # for second model
+    ts = [ pulp.LpVariable(f't_{i}', 0) for i in range(4) ]
+
+    lmb = pulp.LpVariable('lambda', 0, 1)
+    Z1 = pulp.LpVariable('Z1', 0)
+    Z2 = pulp.LpVariable('Z2', 0)
+    X = pulp.LpVariable('X', xmin, xmax)
+    Y = pulp.LpVariable('Y', ymin, ymax)
+
+    # objective function
+    prob += lmb
+
+    # restrictions
+    prob += U1 * lmb - L1 * lmb <= U1 - Z1
+    prob += U2 * lmb - L2 * lmb <= U2 - Z2
+
+    # from Z1 model
+    for p, q, x, y, w in zip(ps, qs, xs_near, ys_near, ws_near):
+        prob += p >= x - X
+        prob += p >= X - x
+        prob += q >= y - Y
+        prob += q >= Y - y
+        prob += w * p + w * q <= Z1
+
+    # from Z2 model
+    prob += Z2 == ts[0] + ts[1] + ts[2] + ts[3]
+    
+    prob += ts[0] >= X - xca
+    prob += ts[0] >= xca - X
+    prob += ts[1] >= Y - yca
+    prob += ts[1] >= yca - Y
+
+    prob += ts[2] >= X - xcb
+    prob += ts[2] >= xcb - X
+    prob += ts[3] >= Y - ycb
+    prob += ts[3] >= ycb - Y
+
+    prob.solve(pulp.PULP_CBC_CMD(msg=False))
+    assert pulp.LpStatus[prob.status] == 'Optimal'
+
+    return PT(X.varValue, Y.varValue, w=0, kind='FussyModel'), Z1.varValue + Z2.varValue
 
 def evalZ1(near: List[PT], X: float, Y: float):
     return max(w * (abs(x - X) + abs(y - Y)) for x, y, w, _ in near)
@@ -92,9 +138,12 @@ def solveZ2(xca: float, yca: float, xcb: float, ycb: float, xmin: float, xmax: f
     return X.varValue, Y.varValue
 
 if __name__ == '__main__':
-    near = [ PT(2.5, 2.5, 1), PT(4, 3, 1), PT(7, 2, 4) ]
+    near = [ PT(2.5, 2.5, 1), PT(4, 3, 1), PT(7, 2, 1) ]
     far = [ PT(0, 0, 1), PT(5, 0, 1), PT(5, 5, 1), PT(0, 5, 1) ]
     xmin = ymin = -5
     xmax = ymax = 10
 
-    optimize(near, far, xmin, xmax, ymin, ymax)
+    opt, cost = optimize(near, far, xmin, xmax, ymin, ymax)
+
+    print(opt)
+    print(cost)
